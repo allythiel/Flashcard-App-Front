049117a9cbc5529a3b86b30b5e4020ba4e332f49
allythiel <allythiel@gmail.com>
2021-07-05 15:13:55 -0500

    Create Index file

---
+++ flashcard-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react';
+
+const jsxElement = <h1>Flashcard App</h1>;
+console.log(jsxElement);
+
+ReactDOM.render(<App />, document.getElementById('root'));
