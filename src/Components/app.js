96f2cd9a895bdbcce9e125e75225d8bccc6f11e3
allythiel <allythiel@gmail.com>
2021-07-05 15:44:49 -0500

    Add Get Request with Axios to obtain Flashcards from Backend

--- src/Components/app.js
+++ src/Components/app.js
@@ -1,4 +1,6 @@
+import { assertExpressionStatement } from '@babel/types';
 import React, { Component } from 'react';
+import NavBar from './NavigationBar/navigationbar';
 
 class App extends Component {
     constructor(props) {
@@ -9,11 +11,19 @@ class App extends Component {
     }
 
     componentDidMount(){
-        this.getAllFlashcards();
+        this.getAllCards();
+    }
+
+    async getAllCards(){
+        let result = await assertExpressionStatement.get("http://localhost:5000/api/collections");
+        this.setState({
+            flashcardArr: result.data
+        });
     }
     render() {
         return (
             <div className="container-fluid">
+                <NavBar />
 
             </div>
         );
